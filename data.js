a135c6f9f8ec521074263d9fcea8237c51619d54
knight1972001 <60019805+knight1972001@users.noreply.github.com>
2020-08-15 01:09:11 -0400

    first up

---
+++ data.js
@@ -0,0 +1,295 @@
+var mousseCake = [{
+        name: 'none',
+        description: 'none',
+        price: '$'
+    },
+    {
+        name: 'Peach chamomile Mousse Cake',
+        description: 'This cake contain fresh Peach. It has flavour of Creamy Peach Mousse and melt in your mouth immediately',
+        price: '$10'
+    },
+    {
+        name: 'Raspberry Mousse Cake',
+        description: 'This one is a mini version of popular charlotte cake. It is not too sweet but it will attract your taste',
+        price: '$12'
+    },
+    {
+        name: 'Strawberry GreenTea Mousse Cake',
+        description: 'With a special combination of strawberry and green tea, the cake will give you a special flavor',
+        price: '$12'
+    },
+    {
+        name: 'Matcha Avocado Lemon Mousse Cake',
+        description: 'Delicious toasted almond crust topped with creamy matcha, avocado, and lemon layer.',
+        price: '$12'
+    },
+    {
+        name: '"Egg" Mousse Cake',
+        description: 'The cream layers are luxurious and rich, but not greasy and heavy like simplified versions made with whipping cream.',
+        price: '$10'
+    },
+    {
+        name: 'Raspberry and Dragon Fruit Mousse Cake',
+        description: 'Melt-in-your-mouth and sweet, with a dark chocolate drizzle, they get their amazing mousse-like texture from cashews and their flavor from fresh, juicy dragon fruit and raspberries.',
+        price: '$12'
+    },
+    {
+        name: 'Apple Mousse and Ricotta Cake',
+        description: 'It is not only a wonderful way to use cooking apples or sharp-flavored eating apples, but the dessert also is a light yet delicious dish, which is great for lunchboxes, picnics, parties and anytime a light sweet is called for.',
+        price: '$10'
+    },
+    {
+        name: 'Banana Mousse Cake',
+        description: 'They are the most nutritionally-dense fruit you can eat. And they are delicious.',
+        price: '$12'
+    },
+    {
+        name: 'Banofee Mousse Cake',
+        description: 'This dessert is sweet yet not too rich. The dulce de leche is evenly balanced out with some cream cheese which you don’t really taste, but it helps cut the sweetness in the mousse.',
+        price: '$10'
+    },
+    {
+        name: 'Blueberry Mousse Cake',
+        description: 'These little cakes might be small, but they are immensely impressive, mainly due to the perfectly shiny layer of vibrant purple mirror glaze that enrobes them. It’s a pretty time-consuming process, but split over a few days it is more than manageable.',
+        price: '$10'
+    },
+    {
+        name: 'Brownie Bottom Chocolate Mousse Cake',
+        description: 'A Brownie Bottom Chocolate Mousse Cake with chocolate whipped cream, to be exact. A triple layer chocolate cake that is ideal for any celebration.Plus chocolate and strawberries are a heavenly combination.',
+        price: '$12'
+    },
+    {
+        name: 'Caramel and Coffee Mousse Cake',
+        description: 'This fancy mousse cake dessert has the most delicious and yet easy to make combination of flavors and textures. You have the sheet cake as a base with a touch of almond for a tastier and more moist consistency.',
+        price: '$12'
+    },
+    {
+        name: 'Chocolate Mousse Cake',
+        description: 'This is the perfect Chocolate Mousse Cake recipe. Soft and moist chocolate cake layer topped with super creamy chocolate mousse and soft chocolate ganache.',
+        price: '$12'
+    },
+    {
+        name: 'Chocolate Mousse Cake Plated Cravings',
+        description: 'Chocolate Mousse Cake is every chocolate lover’s dream! This no-bake dessert is perfect for special occasions but so easy and quick to make from scratch. This easy mousse cake recipe will is a holiday favorite in our family!',
+        price: '$12'
+    },
+    {
+        name: 'Cut Melon Cake (Sliced)',
+        description: 'The cake gives a sweet taste of spring. The natural sweetness from melon will draw you in the first time you see the cake. The cake is definitely one of the cakes you must try at our store',
+        price: '$12'
+    },
+    {
+        name: 'Dairy free strawberry Mousse Cake',
+        description: 'Our dairy-free strawberry mousse cake is light and fruity, made with coconut milk and plenty of strawberries to make a quick and easy cake that everyone can enjoy.You wouldn’t believe that this mousse cake is dairy-free as its light and creamy just like traditional mousse. The sharp strawberry flavour works wonders with the subtle coconut flavour.',
+        price: '$12'
+    },
+    {
+        name: 'Erdbeer Mousse Torte',
+        description: 'This is a cake that originated in Germany. The crust is wonderfully soft, the superfat yogurt filling and the premium strawberry mousse melts gently on the tongue. The bottom part is mostly small strawberries. What an achievement.',
+        price: '$30'
+    },
+    {
+        name: 'Frederikke Waerens Oreo Cheese Cake',
+        description: 'This Mousse Cake is a delicious dessert recipe that combines all your favorite desserts! It starts with a layer of fudgy brownie on the bottom, then a layer of Oreo cheesecake, a chocolate chip cookie mousse and topped with whipped cream.',
+        price: '$30'
+    },
+    {
+        name: 'Strawberry Mousse Cake',
+        description: 'This Strawberry Mousse Cake is a showstopper. The fluffy sponge cake layers are brushed with a strawberry liqueur syrup and laced with a creamy and luscious strawberry mousse frosting in between the cake layers.',
+        price: '$30'
+    },
+    {
+        name: 'Oreo Brookie Mousse Cake(Full)',
+        description: 'This Oreo Brookie Mousse Cake starts with a fudgy brownie, then a layer of Oreo cheesecake, a chocolate chip cookie mousse and topped with whipped cream. Let me tell you this is DELICIOUS!',
+        price: '$35'
+    },
+    {
+        name: 'Oreo Brookie Mousse Cake(Sliced)',
+        description: 'This Oreo Brookie Mousse Cake starts with a fudgy brownie, then a layer of Oreo cheesecake, a chocolate chip cookie mousse and topped with whipped cream. Let me tell you this is DELICIOUS!',
+        price: '$12'
+    },
+    {
+        name: 'Neapolitan Mousse Cake',
+        description: 'Inspired by the tri-colour Neapolitan ice cream, with its signature chocolate-vanilla-strawberry flavour combination. Neapolitan ice cream takes its name from the city of Naples in Italy, and the ice cream has three layers because it resembles the tri-colour Italian flag.',
+        price: '$12'
+    },
+    {
+        name: 'Cream Chocolate Mousse Cake',
+        description: 'This super moist dark chocolate mousse cake combines unsweetened natural cocoa powder and dark cocoa powder for an extra rich flavor. Fill the cake with a simplified chocolate mousse and cover the whole dessert with semi-sweet chocolate ganache.',
+        price: '$30'
+    },
+    {
+        name: 'Purple sweet Potato Mousse Cake',
+        description: 'This special mousse cake is bright and vibrant both in color and flavor! A whipped tofu cream cheese filling and a purple frosting make for a unique cake!',
+        price: '$15'
+    },
+    {
+        name: 'Raspberry Caramel Mousse Cake',
+        description: 'It’s not only beautiful but also delicious! This no-bake cheesecake mousse cake is made with a graham cracker crust, a surprise raspberry jello center surrounded by velvety caramel mousse and garnished with dollops of whipped cream and chocolate garnishes.',
+        price: '$25'
+    },
+    {
+        name: 'Raspberry Mousse Red Velvet Cake',
+        description: 'Topped with fluffy meringues and edible flowers, this pretty-in-pink raspberry velvet cake is about as springy as it gets.',
+        price: '$15'
+    }
+];
+
+var cheeseCake = [{
+        name: 'none',
+        description: 'none',
+        price: '$'
+    },
+    {
+        name: 'Banoffee Cheese Cake',
+        description: 'A delicious twist on Banoffee Pie—a shortbread crust filled with banana cheesecake and topped with a generous pour of toffee sauce! It has all the elements of the classic, but with a decadent twist.',
+        price: '$12'
+    },
+    {
+        name: 'Blueberry Creme Friache Cheese Cake',
+        description: 'This light and silky cheesecake gets its vibrant purple color from roasted blueberries; with a vanilla cookie crust and a delicate crème fraîche glaze, it might be your new favorite cheesecake!',
+        price: '$12'
+    },
+    {
+        name: 'Traditional Cheese Cake',
+        description: "This is a classic cheesecake -- velvety, rich, sweet and so satisfying. We are major fans of a thin sour cream cap on top of cheesecakes; it's the perfect balance to this rich cake.",
+        price: '$35'
+    },
+    {
+        name: 'Fruit Cheese Cake (Sliced)',
+        description: 'Whether you are looking for a special dessert for Easter dinner or just want something a little extra special for guests this weekend, this Creamy New York Cheesecake with Fresh Fruit is the perfect addition to your menu! This rich and creamy cheesecake is all sorts of decadent and the addition of fresh fruit adds the perfect amount of freshness for balance.',
+        price: '$12'
+    },
+    {
+        name: 'Cherry Cheese Cake',
+        description: 'It is the perfect food after all. Creamy and light, full of delicious flavors ranging from fruit to chocolate. It’s sheer perfection',
+        price: '$12'
+    },
+    {
+        name: 'Raspberry Cheese Cake',
+        description: 'This No Bake Raspberry Cheesecake recipe comes together quickly and can be made ahead of time. The combination of berries and cream is a delicious treat. It’s a little taste of summer.',
+        price: '$12'
+    },
+    {
+        name: 'Blueberry Cheese Cake (Small size)',
+        description: 'Blueberry puree into cheesecake batter isn\'t only beautiful,it\'s extremely delicious. The cheesecake stays extra-creamy and has the perfect tartness to it that will have you saying, "just one more bite," another thousand times',
+        price: '$12'
+    },
+    {
+        name: 'Mango Swirl Cheese Cake',
+        description: "When you're looking for the smoothest, most scrumptious cheesecake, the exotic flavor of this mango cheesecake recipe is hard to beat. It's irresistible and beautiful, even giving you the chance to play with decorative swirls that will impress anyone. This is the perfect dessert for family dinners and small parties, and everyone will love it.",
+        price: '$12'
+    },
+    {
+        name: 'Oreo Cheese Cake (Sliced)',
+        description: 'This Oreo Cheesecake is thick, creamy and filled with cookies and cream! It’s baked in an Oreo crust and topped with white chocolate ganache and homemade whipped cream! With the amount of Oreos baked into it, this is the BEST Oreo Cheesecake you’ll ever have!',
+        price: '$12'
+    },
+    {
+        name: 'Red Velvet Cheese Cake',
+        description: 'If you love both red velvet cake and cheesecake, this cake is for you. A red velvet filling is spiked with cocoa, topped with a creamy cheesecake layer, and baked in a dark chocolate crust–making a wickedly-delicious combination.',
+        price: '$12'
+    },
+    {
+        name: 'Traditional Cheese Cake with Cream Blueberry',
+        description: 'This blueberry cheesecake starts with a buttery graham cracker crust, a creamy cheesecake center, and a tangy blueberry swirl. This cheesecake is incredible!',
+        price: '$12'
+    },
+    {
+        name: 'Traditional Cheese Cake with Strawberry',
+        description: "This Strawberry Cheesecake features a creamy cheesecake on a graham cracker crust topped with fresh strawberry sauce.It's simple, light, and fresh. You'll probably be able to enjoy more than one slice without feeling bogged down and heavy.",
+        price: '$30'
+    },
+    {
+        name: 'Vegan Blueberry Cheese Cake',
+        description: 'This cake  is completely raw, refined sugar-free and gluten-free too. It’s perfect to serve on warm days as a cool, sweet dessert.',
+        price: '$12'
+    },
+    {
+        name: 'Avocado Cheese Cake',
+        description: 'This creamy avocado cheesecake is a lighter twist on the classic. It doesn’t require any baking and is best enjoyed chilled, making it the perfect dessert to share with friends and family this summer.',
+        price: '$18'
+    }
+];
+
+var tartCake = [{
+        name: 'none',
+        description: 'none',
+        price: '$'
+    },
+    {
+        name: 'Fresh Peach Tart (3pcs)',
+        description: 'If you love fresh, ripe peaches, then this Fresh Peach Tart is for you! The peaches aren’t cooked in the oven, so they keep their wonderful, just-picked tasted and texture. Juicy peaches + buttery tart dough = peach perfection!',
+        price: '$15'
+    },
+    {
+        name: 'Fruit Egg Tart (Full Size)',
+        description: 'Fresh, colorful, and bursting with juicy fruit, rich pastry cream, a deliciously sweet pastry crust, and an easy fruit tart glaze, this French fruit tart recipe makes a showstopper dessert that is perfect for Spring & Summer!',
+        price: '$20'
+    },
+    {
+        name: 'Blueberry and Lemon Verbena Tarts (3 pcs)',
+        description: 'The combination of light, fragrant lemon verbena custard and blueberries makes for a memorable dessert everyone will love. This cake is especially for vegan',
+        price: '$18'
+    },
+    {
+        name: 'Fresh Berry Mascarpone Tarts (3pcs)',
+        description: 'A Berry Mascarpone Tart is a simple, delicious way to show off all the season’s best berries. A creamy mascarpone filling, a hint of fresh lemon, and four kinds of berries, all on top of a sweet shortbread crust!',
+        price: '$18'
+    },
+    {
+        name: 'Fruit Tarts (Full Size)',
+        description: 'This classic tart has three components, a fluted edged sweet pastry crust, a luscious pastry cream, and glistening rows of fresh fruit. It looks enticing whether you use a single fruit or a medley of fruits and berries.',
+        price: '$12'
+    },
+    {
+        name: 'Gluten Free Lemon (Sliced)',
+        description: 'With an easy press-in almond shortbread crust and luscious, tangy lemon curd, this French-style gluten-free lemon tart is an easier version of the classic. This is the same recipe as my gluten-free lemon bars, just in a different shape!',
+        price: '$15'
+    },
+    {
+        name: 'Creme Brulee Tarts(3 pcs)',
+        description: 'Bites of vanilla-scented creme brulee encased in crisp, golden pastry are a sweet way to end a picnic. This is the special combination that you must try item',
+        price: '$20'
+    },
+    {
+        name: 'Blueberry Traditional Tart',
+        description: "This blueberry tart is an absolute classic and luxurious dessert with rich, creamy vanilla pastry cream in a buttery shortcrust pastry. It's topped with a delicious blueberry filling and garnished with fresh blueberries. A simple and easy recipe that can be done ahead of time",
+        price: '$25'
+    },
+    {
+        name: 'French Tart Flan (piece)',
+        description: "The Parisian flan is a classic French custard pie which I made into a tart. Rich and buttery shortcrust pastry is filled with creamy pastry cream and baked until it is blistered on the top. A decadent dessert that's best made a day ahead which makes it perfect to entertain.",
+        price: '$8'
+    },
+    {
+        name: 'Mini Peach Tarts (3 pcs)',
+        description: "Easy Peach Tarts with fresh fruit, creamy topping, and flakey crust are what dessert should be about. These little bites are perfect for a party or gathering.Simple circles of buttery pastry are baked and then topped with yogurt and peaches. It doesn't get easier than that for a peach pie!",
+        price: '$15'
+    },
+    {
+        name: 'Strawberry Traditional Tart',
+        description: 'Spring is here, and we are embracing a timeless basic today – the Classic French Strawberry Tart, known in French as “Tarte aux Fraises”. This traditional French dessert combines a buttery pastry crust with juicy ripe strawberries arranged on a layer of velvety crème pâtissière.',
+        price: '$12'
+    },
+    {
+        name: 'Chocolate Traditional Tart',
+        description: 'Chocolate Traditional  Tart recipe is what you need if you are craving chocolate. It is a simple and elegant dessert with a rich and decadent chocolate ganache. It is perfect for spoiling your special one on Valentine’s Day, too!',
+        price: '$15'
+    },
+    {
+        name: 'Roasted White Chocolate Caramel Tart (3 pcs)',
+        description: 'Who out there has had the wonderful pleasure of trying caramelized white chocolate!? This stuff is seriously incredible! It’s like caramel and chocolate had a baby together - so so good!',
+        price: '$15'
+    },
+    {
+        name: 'Summer Fruit Tarts (3 pcs)',
+        description: 'Flaky, sweet crust is filled with raspberry custard and topped with an abundance of fresh fruit in this gorgeous fruit tart. This tart is for everyone who love Summer time!',
+        price: '$18'
+    },
+    {
+        name: 'Vegan Panna Cotta Tarts',
+        description: 'It’s delicious, easy to make with crispy homemade pie crust & perfect for a simple dessert! With a fruity pomegranate berry jelly topping, this tart is must-try item in our bakery',
+        price: '$12'
+    }
+];
---
+++ script.js
@@ -0,0 +1,214 @@
+/* Add any JavaScript you need to this file. */
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = 'Time: ' + h + ':' + m + ':' + s;
+    setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
+}
+
+setTimeout(() => {
+    document.querySelector('.overlay').style.display = 'none';
+}, 2000);
+
+//clock
+var c = 0;
+var min = 0;
+
+function myCounter() {
+    if (c < 60 && min === 0) {
+        document.getElementById('demo').innerHTML = 'You accessed website: ' + ++c + 's';
+    } else {
+        if (c < 60) {
+            document.getElementById('demo').innerHTML = 'You accessed website: ' + min + 'm' + ++c + 's';
+        } else {
+            c = 0;
+            min++;
+            document.getElementById('demo').innerHTML = 'You accessed website: ' + min + 'm' + ++c + 's';
+        }
+    }
+}
+
+function allEffect() {
+    startTime();
+    setInterval(myCounter, 1000);
+    document.querySelector('#ShowMousse').onclick = ShowMouse;
+    document.querySelector('#ShowTart').onclick = ShowTart;
+    document.querySelector('#ShowCheese').onclick = ShowCheese;
+    document.querySelector('#orderProblem').onclick = orderProblem;
+}
+
+function ShowMouse() {
+    document.querySelector('.overlay').style.display = 'initial';
+    setTimeout(() => {
+        document.querySelector('.overlay').style.display = 'none';
+    }, 2000);
+
+    let mid = document.getElementById('mid');
+    mid.innerHTML = '';
+    for (let i = 1; i <= 26; i++) {
+        let link = './images/mousse/image (' + i + ').jpg';
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        let form = document.createElement('form');
+        form.setAttribute('action', 'https://formspree.io/xgenqdqp');
+        form.setAttribute('method', 'POST');
+        let img = document.createElement('img');
+        img.setAttribute('id', 'product');
+        img.setAttribute('src', link);
+        img.setAttribute('style', 'width:200px; height: 300px; margin-top: 10px');
+        form.appendChild(img);
+        let h1 = document.createElement('h1');
+        h1.setAttribute('class', 'cakeName');
+        h1.innerText = mousseCake[i].name;
+        form.appendChild(h1);
+
+        let p1 = document.createElement('p');
+        p1.setAttribute('class', 'price');
+        p1.innerText = mousseCake[i].price;
+        form.appendChild(p1);
+
+        let p2 = document.createElement('p');
+        p2.setAttribute('class', 'des');
+        p2.innerText = mousseCake[i].description;
+        form.appendChild(p2);
+
+        let lable = document.createElement('lable');
+        let textarea = document.createElement('textarea');
+        textarea.setAttribute('style', 'display: none');
+        textarea.setAttribute('name', 'message');
+        textarea.innerText = link;
+        lable.appendChild(textarea);
+        form.appendChild(lable);
+        let p = document.createElement('p');
+        let button = document.createElement('button');
+        button.setAttribute('type', 'submit');
+        button.innerText = 'Add to cart';
+        p.appendChild(button);
+        form.appendChild(p);
+        card.appendChild(form);
+        mid.appendChild(card);
+    }
+}
+
+function ShowTart() {
+    document.querySelector('.overlay').style.display = 'initial';
+    setTimeout(() => {
+        document.querySelector('.overlay').style.display = 'none';
+    }, 2000);
+
+    let mid = document.getElementById('mid');
+    mid.innerHTML = '';
+    for (let i = 1; i <= 15; i++) {
+        let link = './images/tart/image (' + i + ').jpg';
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        let form = document.createElement('form');
+        form.setAttribute('action', 'https://formspree.io/xgenqdqp');
+        form.setAttribute('method', 'POST');
+        let img = document.createElement('img');
+        img.setAttribute('id', 'product');
+        img.setAttribute('src', link);
+        img.setAttribute('style', 'width:200px; height: 300px; margin-top: 10px');
+        form.appendChild(img);
+        let h1 = document.createElement('h1');
+        h1.setAttribute('class', 'cakeName');
+        h1.innerText = tartCake[i].name;
+        form.appendChild(h1);
+
+        let p1 = document.createElement('p');
+        p1.setAttribute('class', 'price');
+        p1.innerText = tartCake[i].price;
+        form.appendChild(p1);
+
+        let p2 = document.createElement('p');
+        p2.setAttribute('class', 'des');
+        p2.innerText = tartCake[i].description;
+        form.appendChild(p2);
+
+        let lable1 = document.createElement('lable');
+        let textarea = document.createElement('textarea');
+        textarea.setAttribute('style', 'display: none');
+        textarea.setAttribute('name', 'message');
+        textarea.innerText = link;
+        lable1.appendChild(textarea);
+        form.appendChild(lable1);
+        let p = document.createElement('p');
+        let button = document.createElement('button');
+        button.setAttribute('type', 'submit');
+        button.innerText = 'Add to cart';
+        p.appendChild(button);
+        form.appendChild(p);
+        card.appendChild(form);
+        mid.appendChild(card);
+    }
+}
+
+function ShowCheese() {
+    document.querySelector('.overlay').style.display = 'initial';
+    setTimeout(() => {
+        document.querySelector('.overlay').style.display = 'none';
+    }, 2000);
+
+    let mid = document.getElementById('mid');
+    mid.innerHTML = '';
+    for (let i = 1; i <= 14; i++) {
+        let link = './images/cheese/image (' + i + ').jpg';
+        let card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        let form = document.createElement('form');
+        form.setAttribute('action', 'https://formspree.io/xgenqdqp');
+        form.setAttribute('method', 'POST');
+        let img = document.createElement('img');
+        img.setAttribute('id', 'product');
+        img.setAttribute('src', link);
+        img.setAttribute('style', 'width:200px; height: 300px; margin-top: 10px');
+        form.appendChild(img);
+        let h1 = document.createElement('h1');
+        h1.setAttribute('class', 'cakeName');
+        h1.innerText = cheeseCake[i].name;
+        form.appendChild(h1);
+
+        let p1 = document.createElement('p');
+        p1.setAttribute('class', 'price');
+        p1.innerText = cheeseCake[i].price;
+        form.appendChild(p1);
+
+        let p2 = document.createElement('p');
+        p2.setAttribute('class', 'des');
+        p2.innerText = cheeseCake[i].description;
+        form.appendChild(p2);
+
+        let lable1 = document.createElement('lable');
+        let textarea = document.createElement('textarea');
+        textarea.setAttribute('style', 'display: none');
+        textarea.setAttribute('name', 'message');
+        textarea.innerText = link;
+        lable1.appendChild(textarea);
+        form.appendChild(lable1);
+        let p = document.createElement('p');
+        let button = document.createElement('button');
+        button.setAttribute('type', 'submit');
+        button.innerText = 'Add to cart';
+        p.appendChild(button);
+        form.appendChild(p);
+        card.appendChild(form);
+        mid.appendChild(card);
+    }
+}
+
+function orderProblem() {
+    let order = document.getElementById('orderProblemInput');
+    order.setAttribute('type', 'text');
+}
